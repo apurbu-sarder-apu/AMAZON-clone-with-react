c1d171dd56f276c50a9a84add8f41b82249db210
Apurbu Sarder <apurbusarderjust321@gmail.com>
2021-06-29 13:37:02 +0600

    second commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZf7gVL9ew_QN-nqsMmEN-_DKQ6K8-JJI",
+    authDomain: "challenge-a5dc5.firebaseapp.com",
+    projectId: "challenge-a5dc5",
+    storageBucket: "challenge-a5dc5.appspot.com",
+    messagingSenderId: "849830327929",
+    appId: "1:849830327929:web:7f9a0e150e2d9c833286a8"
+  };
