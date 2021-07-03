e4fcd086c7541b9df87eb84f993525a05dbfd45b
Apurbu Sarder <apurbusarderjust321@gmail.com>
2021-07-03 13:07:52 +0600

    tenth commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,7 +7,7 @@ import CheckoutProduct from './CheckoutProduct';
 
 
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
     return (
         <div className="checkout">
             <div className="checkout__left">
@@ -17,6 +17,7 @@ function Checkout() {
                     alt=""/>
             
             <div>
+                <h3>Hello, {user?.email}</h3>
                 <h2 className="checkout__title">
                     Your shopping Basket
                 </h2>
